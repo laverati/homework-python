9e058538204e06dad33278c96ce49a8619f799f0
Elizabeth <laverati@gmail.com>
2021-09-10 17:17:40 +0300

    0909_homework.py

--- 0909_homework.py
+++ 0909_homework.py
@@ -10,6 +10,15 @@ big_names = ['Сазонова Анастасия Александровна', '
 
 name = 'Mamrenko Elizaveta Vitalievna'
 
+def show(func):
+    def new_func(*args,**kwarg):
+        print('Name of funcion - ' , func.__name__)
+        print('args - ' , args)
+        print('Kwargs - ' , kwarg)
+        result = func(*args, **kwarg)
+        return result
+    return new_func
+
 def initials(name):
     result = name.split()
     return result[0] + ' ' + result[1][0:1] + '. ' + result [2] [0:1] + '.'
@@ -43,4 +52,20 @@ for x in ranger:
 names = ['Mamrenko Elizaveta Vitalievna', 'Mamrenko Elizaveta Vitalievna']
 
 print(gen_names('Mamrenko Elizaveta Vitalievna', 5))
+
+
+@show
+def get_elements_for(list_): # функция вывода элементов массива с буквами через for
+    for k in list_:
+        print(k)
+
+
+print (get_elements_for(['Волков Андрей Дмитриевич', 'Исаков Егор Маркович']))
+
+
+def get_elements_while(list_):
+    k = 0
+    while k < len(list_):
+        print(list_[k])
+        k += 1
 
